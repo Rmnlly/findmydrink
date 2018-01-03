16c59022b479d3ed50c593b05e3432fea7389905
JLim <lim.jess9@gmail.com>
2018-01-03 10:51:24 -0500

    Add sign up processing and modify index

---
+++ process-signup.php
@@ -0,0 +1,27 @@
+<?php
+  session_start();
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $email = $_POST["email"];
+
+include("includes/database.php");
+
+$sql = "INSERT INTO `user` (`id`, `email`, `username`, `password`) VALUES (NULL, '$email', '$username', '$password');";
+
+$stmt = $pdo->prepare($sql);
+
+$result = $stmt->execute();
+
+if($result){
+  $_SESSION["authenticated"] = true;
+  $_SESSION["user_id"] = $row['id'];
+  $_SESSION["username"] = $row['username'];
+  echo 'Thank you for registering with our website. You are now logged in.';
+  ?><a href="index.php">Home</a>
+  <?
+}else{
+  echo("Invalid sign up. Please try again.");
+  ?><a href="signup-form.php">Sign Up</a>
+  <?
+}
+?>
