dc47439346c4efa587885ff504c48b8034fc88dd
Duchess <33904740+Rmnlly@users.noreply.github.com>
2018-01-02 20:15:06 -0500

    created footer, header and the insert pairing pages.

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,10 @@
 <?php  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css">
+    <title></title>
+  </head>
+  <body>
+    <?include("includes/nav.php")?>
---
+++ insert-pairing-form.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+  include("includes/database.php");
+  include("includes/header.php");
+  //The session will contain the users id, we will need to obtain this and pass it
+  //to the processing page through the hidden field defined below
+ ?>
+ <div class="form-container">
+   <form class ="input-idea" action="process-insert-pairing.php" method="post" enctype="multipart/form-data">
+     <input type="hidden" name="user_id" value="<?//Here we will pass the userid?>">
+
+     <label for="food">Food </label>
+     <input id='food' type="text" name="food" maxlength="25"/>
+
+     <label for="drink">Drink </label>
+     <input id='drink' type="text" name="drink" maxlength="25"/>
+
+     <label for="keywords">Keywords </label>
+     <textarea type="tArea" name="keywords" rows="10" cols="16" maxlength="150"></textarea>
+
+     <label for="picture">Pairing Image</label>
+     <input id='picture' type="file" name ="picture"/>
+
+     <label for="private">Is this idea private?</label>
+     <input type="checkbox" name="private" id="private"/>
+
+     <input type="submit" value="Create Pairing">
+
+   </form>
+
+ </div>
+<?
+  include("includes/footer.php");
+?>
