2c1aeacf525746ac7e8ba6240856d28e9007ebb2
JLim <lim.jess9@gmail.com>
2018-01-03 11:26:56 -0500

    Add search function

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ include("includes/header.php");
 include('includes/navbar.php');
 ?>
     <h1>Find your drink</h1>
+    <form name="frmSearch" method="post" action="search.php">
+      <input name="search" type="text" id="search">
+      <input type="submit" value="Search">
+    </form>
+
 <?
   include("includes/footer.php");
 ?>
---
+++ process-search.php
@@ -0,0 +1,22 @@
+<?php
+  include("includes/database.php");
+
+  $search = $_POST['search'];
+
+  $query = "SELECT * FROM `Pairings` WHERE `keywords` LIKE :search OR `food` LIKE :search";
+  $stmt = $dbh->prepare($query);
+  $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_INT);
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+
+  if ($stmt->rowCount() > 0) {
+    $result = $stmt->fetchAll();
+
+    foreach( $result as $row ) {
+    echo $row["id"];
+    echo $row["drink"];
+    }
+    } else {
+    echo 'There is nothing to show';
+  }
+ ?>
