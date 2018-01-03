6bd9c04bbf0e3a5cc8a9edc3b76dbafe9c6b0aed
JLim <lim.jess9@gmail.com>
2018-01-03 13:15:32 -0500

    Edit index and signup form

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ include("includes/header.php");
 include('includes/navbar.php');
 echo($_SESSION['user_id']);
 ?>
-    <h1>Find your drink</h1>
-    <form name="frmSearch" method="post" action="process-search.php">
-      <input name="search" type="text" id="search">
-      <input type="submit" value="Search">
-    </form>
+  <h1>Find your drink</h1>
+  <form name="frmSearch" method="post" action="process-search.php">
+    <input name="search" type="text" id="search">
+    <input type="submit" value="Search">
+  </form>
 
 <?
   include("includes/footer.php");
--- signup-form.php
+++ signup-form.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Sign Up</title>
-  </head>
-  <body>
-    <div class="signup">
-      <form action="process-signup.php" method="post">
-        Username: <input type="text" name="username"/><br/>
-        Password: <input type="text" name="password"/><br/>
-        Email: <input type="text" name="email"/><br/>
-        Submit: <input type="submit"/>
-      </form>
-    </div>
-  </body>
-</html>
+<?php
+include("includes/header.php");
+include('includes/navbar.php');
+?>
+  <div class="signup">
+    <form action="process-signup.php" method="post">
+      Username: <input type="text" name="username"/><br/>
+      Password: <input type="text" name="password"/><br/>
+      Email: <input type="text" name="email"/><br/>
+      Submit: <input type="submit"/>
+    </form>
+  </div>
+<?
+include("includes/footer.php");
+?>
