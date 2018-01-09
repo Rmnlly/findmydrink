e787fe72754c9ac661b2638444c39d21801f2dca
Duchess <33904740+Rmnlly@users.noreply.github.com>
2018-01-08 20:11:05 -0500

    Created and aligned footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,11 @@
 <?php  ?>
 
   </body>
+  <footer>
+    <div class="footer-container">
+      <ul class="footer-info">
+        <li><a href="#">About Us</a></li>
+      </ul>
+    </div>
+  </footer>
 </html>
